29f21464bc28fc13ecabf782bf5cd1317ccf00e2
Rohit More <cserohitmore@gmail.com>
2025-01-17 23:49:55 +0530

    Minor Changes

--- app/about-section/About.tsx
+++ app/about-section/About.tsx
@@ -5,7 +5,7 @@ import AnimatedTitle from "../animations/AnimatedTitle";
 const About = () => {
   return (
     <section
-      className="relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-20 lg:pb-56"
+      className="relative z-10 w-full items-center justify-center overflow-hidden bg-[#0E1016] bg-cover bg-center pt-16 md:pt-20 lg:pt-20"
       id="about"
     >
       <div className="mx-auto flex w-[90%] flex-col items-center justify-center lg:max-w-[1212.8px]">
