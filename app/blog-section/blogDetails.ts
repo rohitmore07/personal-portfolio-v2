35191fa06159774fce9601b5a6da58b4866df0a0
Rohit More <cserohitmore@gmail.com>
2025-01-20 23:51:48 +0530

    Build

--- app/blog-section/blogDetails.ts
+++ app/blog-section/blogDetails.ts
@@ -8,23 +8,6 @@ export type blogProps = {
 };
 
 export const blogDetails = [
-  // {
-  //   title: "Building a Text Field Component with Material UI, Formik, and Yup",
-  //   image:
-  //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1628442954056/OHNFYAzz4.jpeg?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-  //   date: "12/31/2022",
-  //   url: "",
-  //   available: false,
-  // },
-  // {
-  //   title:
-  //     "Creating a Select Field Component with Material UI, Formik, and Yup",
-  //   image:
-  //     "https://cdn.hashnode.com/res/hashnode/image/upload/v1624616590157/N4Hvd8NfZ.png?w=1600&h=840&fit=crop&crop=entropy&auto=compress,format&format=webp",
-  //   date: "12/31/2022",
-  //   url: "",
-  //   available: false,
-  // },
   {
     title:
       "All About Threejs",
